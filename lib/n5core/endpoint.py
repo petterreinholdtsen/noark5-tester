bb70bf40698866b408a24e57b70beff2e2fdd3b8
Petter Reinholdtsen <pere@hungry.com>
2018-09-07 19:24:58 +0200

    Implement oauth login support.

--- lib/n5core/endpoint.py
+++ lib/n5core/endpoint.py
@@ -16,8 +16,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+import base64
 import json
 import mechanize
+import urllib
 import urllib2
 import urlparse
 
