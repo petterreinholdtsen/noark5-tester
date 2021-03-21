7a83c22f61e65376295dda085415d8ab853dfc57
Petter Reinholdtsen <pere@hungry.com>
2021-03-21 09:33:28 +0100

    Revert "Try with Content-Length as a string, in case that work better."
    Did not make any difference, so no use keeping it.
    
    This reverts commit cada409a1ea6ea25677b41643eb763537334ee17.

--- lib/n5core/endpoint.py
+++ lib/n5core/endpoint.py
@@ -161,7 +161,7 @@ Recursively look for relation in API.
 
         if data is not None and length is 0:
             length = len(data)
-        headers['Content-Length'] = str(length)
+        headers['Content-Length'] = length
         if hasattr(self, 'token'):
             headers['Authorization'] = self.token
         if self.verbose:
